3154678e11f7a430a937b922dcbd07c62cdf4d16
onomlight <pass041@nate.com>
2023-02-16 20:57:00 +0900

    a 태그로 연결하였다가 Link로 연결하는법 확인

--- src/pages/news/index.js
+++ src/pages/news/index.js
@@ -1,8 +1,23 @@
 // our-domain.com
 // our-domain.com/news/something-important
 
+import Link from 'next/link'
+import { Fragment } from 'react'
+
 function NewsPage() {
-  return <h1>The NewsPage Page</h1>
+  return (
+    <Fragment>
+      <h1>The NewsPage Page</h1>
+      <ul>
+        <li>
+          <Link href="/new/nextjs-is-a-great-framework">
+            넥스트제이에스연습
+          </Link>
+        </li>
+        <li>SomeTingElse</li>
+      </ul>
+    </Fragment>
+  )
 }
 
 export default NewsPage
