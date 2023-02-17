94b190daff74981f44532702367f9bf963229e03
onomlight <pass041@nate.com>
2023-02-18 00:03:45 +0900

    더미데이터 콘솔로그 출력 연동

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,1 +1,12 @@
 // our-domain.com/new-meetup
+import NewMeetupForm from '../../components/meetups/NewMeetupForm'
+
+function NewMeetupPage() {
+  function addMeetupHndler(enteredMeetupDate) {
+    console.log(enteredMeetupDate)
+  }
+  return <NewMeetupForm onAddMeetup={addMeetupHndler} />
+}
+
+export default NewMeetupPage
+// 이럴경우 콘솔로그된 데이터가 보임
