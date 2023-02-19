1c17c2c0b060711848bcd852787ee76d8017ba14
onomlight <pass041@nate.com>
2023-02-19 21:09:15 +0900

    몽고DB 작업 및 API 요청하기

--- components/ui/Card.js
+++ components/ui/Card.js
@@ -1,7 +1,75 @@
-import classes from './Card.module.css';
+import { MongoClient, ObjectId } from 'mongodb'
+import { Fragment } from 'react'
+import Head from 'next/head'
 
-function Card(props) {
-  return <div className={classes.card}>{props.children}</div>;
+import MeetupDetail from '../../components/meetups/MeetupDetail'
+
+function MeetupDetails(props) {
+  return (
+    <Fragment>
+      <Head>
+        <title>{props.meetupData.title}</title>
+        <meta name="description" content={props.meetupData.description} />
+      </Head>
+      <MeetupDetail
+        image={props.meetupData.image}
+        title={props.meetupData.title}
+        address={props.meetupData.address}
+        description={props.meetupData.description}
+      />
+    </Fragment>
+  )
 }
 
-export default Card;
+export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    'mongodb+srv://maximilian:TU6WdZF2EjFWsqUt@cluster0.ntrwp.mongodb.net/meetups?retryWrites=true&w=majority',
+  )
+  const db = client.db()
+
+  const meetupsCollection = db.collection('meetups')
+
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray()
+
+  client.close()
+
+  return {
+    fallback: 'blocking',
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
+  }
+}
+
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  const meetupId = context.params.meetupId
+
+  const client = await MongoClient.connect(
+    'mongodb+srv://maximilian:TU6WdZF2EjFWsqUt@cluster0.ntrwp.mongodb.net/meetups?retryWrites=true&w=majority',
+  )
+  const db = client.db()
+
+  const meetupsCollection = db.collection('meetups')
+
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: ObjectId(meetupId),
+  })
+
+  client.close()
+
+  return {
+    props: {
+      meetupData: {
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        image: selectedMeetup.image,
+        description: selectedMeetup.description,
+      },
+    },
+  }
+}
+
+export default MeetupDetails
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,66 @@
 // 아직 하위 폴더가 없어서 our-domain.com 으로 부를것
 // 이름을 바꿀수없는건 pages 폴더뿐
 
-import { useEffect, useState } from 'react'
+import { Fragment } from 'react'
+import Head from 'next/head'
+import { MongoClient } from 'mongodb'
+
 import MeetupList from '../components/meetups/MeetupList'
 
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: ' A first meet up ',
-    imge:
-      'https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__340.jpg',
-    address: ' 1234567',
-    description: ' this is 1',
-  },
-  {
-    id: 'm2',
-    title: ' A 2 meet up ',
-    imge:
-      ' https://cdn.pixabay.com/photo/2020/06/02/06/52/cat-5249722__480.jpg',
-    address: ' 1234567',
-    description: ' this is 2',
-  },
-]
-function HomePage() {
-  const [loadedMeetups, setLoadedMeetups] = useState([])
-  useEffect(() => {
-    // http 요청
-    setLoadedMeetups(DUMMY_MEETUPS)
-  }, [])
-  return <MeetupList meetups={loadedMeetupsS} />
+function HomePage(props) {
+  return (
+    <Fragment>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name="description"
+          content="Browse a huge list of highly active React meetups!"
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />;
+    </Fragment>
+  )
 }
+
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   // fetch data from an API
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS
+//     }
+//   };
+// }
+
+export async function getStaticProps() {
+  // 비동기처리로 진행
+  // fetch data from an API
+  const client = await MongoClient.connect(
+    'mongodb+srv://maximilian:TU6WdZF2EjFWsqUt@cluster0.ntrwp.mongodb.net/meetups?retryWrites=true&w=majority',
+  )
+  const db = client.db()
+
+  const meetupsCollection = db.collection('meetups')
+
+  const meetups = await meetupsCollection.find().toArray()
+
+  client.close()
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 1,
+    // 1초마다 페이지를 다시생성 한시간마다 3600
+  }
+}
+
 export default HomePage
